dead82e13490b51f8aca570a59f74d2b65ef6ef1
Rustam Karimov <rkarimov.r@gmail.com>
2020-05-11 18:41:35 +0400

    initial implementation

--- src/main/java/org/example/CoronaDisinfector.java
+++ src/main/java/org/example/CoronaDisinfector.java
@@ -2,8 +2,14 @@ package org.example;
 
 public class CoronaDisinfector {
 
+    private Announcer announcer = new ConsoleAnnouncer();
+    private Policeman policeman = new PolicemanImpl();
+
     public void start(Room room) {
+        announcer.announce("disinfect begin!");
+        policeman.makePeopleLeaveRoom();
         disinfect(room);
+        announcer.announce("can back hoom");
     }
 
     private void disinfect(Room room) {
